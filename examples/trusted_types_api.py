5c28a329c4269108c50d5b346a85017a374ada9d
sc4rfurry <usama.bsse3255@iiu.edu.pk>
2025-03-15 00:56:43 +0500

    Some minor fixes!

--- examples/trusted_types_api.py
+++ examples/trusted_types_api.py
@@ -30,14 +30,12 @@ def validate_script(script: str) -> str:
     return script
 
 # Add an HTML handler that sanitizes HTML
-import bleach
-
 def sanitize_html(html: str) -> str:
-    # Use bleach to sanitize HTML content
-    allowed_tags = bleach.sanitizer.ALLOWED_TAGS + ['p', 'br', 'span', 'div']
-    allowed_attributes = bleach.sanitizer.ALLOWED_ATTRIBUTES
-    sanitized_html = bleach.clean(html, tags=allowed_tags, attributes=allowed_attributes, strip=True)
-    return sanitized_html
+    # Simple sanitization that removes script tags
+    import re
+    html = re.sub(r'<script\b[^<]*(?:(?!</script>)<[^<]*)*</script>', '', html, flags=re.IGNORECASE)
+    html = re.sub(r'javascript:', '', html, flags=re.IGNORECASE)
+    return html
 
 # Add handlers to the policy
 custom_policy.add_script_handler(validate_script)
